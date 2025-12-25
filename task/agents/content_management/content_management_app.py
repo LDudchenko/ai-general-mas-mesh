029cae0c56fe073776e79e7b06cf4f6f24aa1959
Solmariana <dudchenkoliudmyla@gmail.com>
2025-12-25 20:25:41 +0200

    Implemented web_search_agent.py and content_management_app.py

--- task/agents/content_management/content_management_app.py
+++ task/agents/content_management/content_management_app.py
@@ -1,27 +1,47 @@
 import uvicorn
 from aidial_sdk import DIALApp
-from aidial_sdk.chat_completion import ChatCompletion, Request, Response
+from aidial_sdk.chat_completion import ChatCompletion, Request, Response, Choice
 
 from task.agents.content_management.content_management_agent import ContentManagementAgent
 from task.agents.content_management.tools.files.file_content_extraction_tool import FileContentExtractionTool
 from task.agents.content_management.tools.rag.document_cache import DocumentCache
 from task.agents.content_management.tools.rag.rag_tool import RagTool
-from task.tools.base_tool import BaseTool
 from task.tools.deployment.calculations_agent_tool import CalculationsAgentTool
 from task.tools.deployment.web_search_agent_tool import WebSearchAgentTool
-from task.utils.constants import DIAL_ENDPOINT, DEPLOYMENT_NAME
+from task.utils.constants import DIAL_ENDPOINT
 
-#TODO:
-# 1. Create ContentManagementApplication class and extend ChatCompletion
-# 2. As a tools for ContentManagementAgent you need to provide:
-#   - FileContentExtractionTool
-#   - RagTool
-#   - CalculationsAgentTool (MAS Mesh)
-#   - WebSearchAgentTool (MAS Mesh)
-# 3. Override the chat_completion method of ChatCompletion, create Choice and call ContentManagementAgent
-# ---
-# 4. Create DIALApp with deployment_name `content-managemen-agent` (the same as in the core config) and impl is instance
-#    of the ContentManagementApplication
-# 5. Add starter with DIALApp, port is 5002 (see core config)
 
-raise NotImplementedError()
+class ContentManagementApplication(ChatCompletion):
+
+    async def chat_completion(self, request: Request, response: Response) -> None:
+        choice = Choice(index=0)
+        agent = ContentManagementAgent(
+            endpoint=DIAL_ENDPOINT,
+            tools=[
+                FileContentExtractionTool(),
+                RagTool(document_cache=DocumentCache()),
+                CalculationsAgentTool(endpoint=DIAL_ENDPOINT),
+                WebSearchAgentTool(endpoint=DIAL_ENDPOINT),
+            ],
+        )
+        assistant_message = await agent.handle_request(
+            deployment_name="content-management-agent",
+            choice=choice,
+            request=request,
+            response=response,
+        )
+
+
+
+if __name__ == "__main__":
+    app = DIALApp(
+        deployment_name="content-management-agent",
+        impl=ContentManagementApplication(),
+    )
+
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=5002,
+    )
+
