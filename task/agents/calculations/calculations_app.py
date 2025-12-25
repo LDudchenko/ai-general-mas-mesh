e1ccc88969843213abd9a9e1e67fa338b7204dfe
Solmariana <dudchenkoliudmyla@gmail.com>
2025-12-25 20:22:00 +0200

    Implemented calculations_app.py

--- task/agents/calculations/calculations_app.py
+++ task/agents/calculations/calculations_app.py
@@ -1,29 +1,44 @@
-import os
 
 import uvicorn
 from aidial_sdk import DIALApp
-from aidial_sdk.chat_completion import ChatCompletion, Request, Response
+from aidial_sdk.chat_completion import ChatCompletion, Request, Response, Choice
 
 from task.agents.calculations.calculations_agent import CalculationsAgent
 from task.agents.calculations.tools.simple_calculator_tool import SimpleCalculatorTool
-from task.tools.base_tool import BaseTool
 from task.agents.calculations.tools.py_interpreter.python_code_interpreter_tool import PythonCodeInterpreterTool
 from task.tools.deployment.content_management_agent_tool import ContentManagementAgentTool
 from task.tools.deployment.web_search_agent_tool import WebSearchAgentTool
 from task.utils.constants import DIAL_ENDPOINT, DEPLOYMENT_NAME
 
+class CalculationsApplication(ChatCompletion):
 
-#TODO:
-# 1. Create CalculationsApplication class and extend ChatCompletion
-# 2. As a tools for CalculationsAgent you need to provide:
-#   - SimpleCalculatorTool
-#   - PythonCodeInterpreterTool
-#   - ContentManagementAgentTool (MAS Mesh)
-#   - WebSearchAgentTool (MAS Mesh)
-# 3. Override the chat_completion method of ChatCompletion, create Choice and call CalculationsAgent
-# ---
-# 4. Create DIALApp with deployment_name `calculations-agent` (the same as in the core config) and impl is instance of
-#    the CalculationsApplication
-# 5. Add starter with DIALApp, port is 5001 (see core config)
+    async def chat_completion(self, request: Request, response: Response) -> None:
+        choice = Choice(index=0)
+        agent = CalculationsAgent(
+            endpoint=DIAL_ENDPOINT,
+            tools=[
+                SimpleCalculatorTool(),
+                PythonCodeInterpreterTool(),
+                ContentManagementAgentTool(endpoint=DIAL_ENDPOINT),
+                WebSearchAgentTool(endpoint=DIAL_ENDPOINT),
+            ],
+        )
+        assistant_message = await agent.handle_request(
+            deployment_name=DEPLOYMENT_NAME,
+            choice=choice,
+            request=request,
+            response=response,
+        )
 
-raise NotImplementedError()
+
+if __name__ == "__main__":
+    app = DIALApp(
+        deployment_name="calculations-agent",
+        impl=CalculationsApplication(),
+    )
+
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=5001,
+    )
